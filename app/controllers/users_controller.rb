6f596381fa7b1d1d3ccc69652d3466a7e3cc034c
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-09-05 17:55:39 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def edit
+   endindex.json.jbuilder
   end
 
   def update
@@ -7,6 +8,15 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
